796cb2fbabfc74d009ae709d16e5c537741354a2
Huynh Nguyễn <hv198197@gmail.com>
2020-08-11 00:00:28 +0700

    nhon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import TitleBar from "./components/title-bar/title-bar";
+import Article from "./components/article/article";
+import { Row, Col, Button, Divider } from "antd";
+import { DeleteFilled, HeartFilled, SettingFilled } from "@ant-design/icons";
+
+const randomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomDataFromAPI = async () => {
+  const page = randomInteger(1, 200);
+  const res = await fetch(
+    `https://www.cooky.vn/directory/search?q=null&st=3&lv=2&cs=&cm=&dt=&igt=&oc=&p=&crs=4&page=${page}&pageSize=&append=true&video=false`
+  );
+  const list = (await res.json()).recipes;
+  const item = list[randomInteger(0, list.length - 1)];
+  if (item) {
+    const resDetail = await fetch(
+      `https://marketapi.cooky.vn/recipe/v1.3/detail?id=${item.Id}&checksum=2fec6cab3ba18b02adfa5dd600482931`
+    );
+    const jsonDetail = await resDetail.json();
+    if (jsonDetail.message === "Success") {
+      return jsonDetail.data;
+    }
+  }
+  return;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (!data) {
+      getRandomDataFromAPI().then((res) => {
+        setData(res);
+      });
+    }
+  });
+
+  if (data) {
+    return (
+      <Row justify="center">
+        <Col xs={22} sm={18} md={14} lg={12}>
+          <Article data={data}></Article>
+          <div className="action-bar">
+            <Row>
+              <Col>
+                <Button
+                  style={{ background: "#F44336", border: "#F44336 solid 1px" }}
+                  type="primary"
+                  shape="circle"
+                  icon={<HeartFilled />}
+                />
+                <Divider type="vertical"></Divider>
+                <Button type="primary" shape="circle" icon={<DeleteFilled />} />
+                <Divider type="vertical"></Divider>
+                <Button
+                  style={{
+                    background: "rgb(44 44 44)",
+                    border: "rgb(44 44 44) solid 1px",
+                  }}
+                  type="primary"
+                  shape="circle"
+                  icon={<SettingFilled />}
+                />
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
---
+++ src/components/article/article.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./article.scss";
+import {
+  ClockCircleOutlined,
+  UserOutlined,
+  CaretRightOutlined,
+} from "@ant-design/icons";
+import { Row, Col, Button, Divider, List, Collapse } from "antd";
+
+const { Panel } = Collapse;
+
+function Article({ data }) {
+  console.log(data);
+  return (
+    <div className="article">
+      <h1>{data.name}</h1>
+      <Row justify="center">
+        <Col>
+          <Button type="text">
+            <ClockCircleOutlined /> {data.totalTime}
+            {" phút"}
+          </Button>
+          <Divider type="vertical" />
+          <Button type="text">
+            <UserOutlined /> {data.servings}
+            {" người"}
+          </Button>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <img src={data.photos[0][5].url} className="img-preview"></img>
+      </Row>
+      <Row>
+        <div dangerouslySetInnerHTML={{ __html: data.description }}></div>
+      </Row>
+      <Row>
+        <List
+          className="list-ing"
+          header={<div>Thành Phần:</div>}
+          bordered
+          dataSource={data.ingredients}
+          renderItem={(item) => (
+            <List.Item>
+              <h3>{item.name}</h3>
+              <h3>
+                <span
+                  dangerouslySetInnerHTML={{ __html: item.quantity }}
+                ></span>{" "}
+                {item.unit.unit}
+              </h3>
+            </List.Item>
+          )}
+        />
+      </Row>
+      <Divider>
+        <h3>Cách Thực Hiện</h3>
+      </Divider>
+      <Row>
+        <Collapse
+          defaultActiveKey={data.steps.map((x, i) => i)}
+          expandIcon={({ isActive }) => (
+            <CaretRightOutlined rotate={isActive ? 90 : 0} />
+          )}
+        >
+          {data.steps.map((s, i) => {
+            return (
+              <Panel header={"Bước " + (i + 1) + ": " + s.content} key={i}>
+                {s.photos.map((p) => {
+                  return <img className="img-step" src={p[3].url}></img>;
+                })}
+              </Panel>
+            );
+          })}
+        </Collapse>
+      </Row>
+      <Divider></Divider>
+      <Row justify="end">
+        <i>Nguồn: Cooky.vn</i>
+      </Row>
+    </div>
+  );
+}
+
+export default Article;
